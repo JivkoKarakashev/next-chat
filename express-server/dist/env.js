e3a30504897fbc00bb8da1e5752b03e55afab9bb
JivkoKarakashev <86545491+JivkoKarakashev@users.noreply.github.com>
2026-02-23 15:48:52 +0200

    add env config file

---
+++ express-server/dist/env.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.env = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+const nodeEnv = process.env.NODE_ENV || 'development';
+const envFile = `.env.${nodeEnv}`;
+dotenv_1.default.config({
+    path: path_1.default.resolve(process.cwd(), envFile)
+});
+if (!process.env.POSTGRES_URL) {
+    throw new Error(`POSTGRES_URL is missing in ${envFile}!`);
+}
+console.log('Using DB host:', new URL(process.env.POSTGRES_URL).hostname);
+console.log("DB URL OK:", !!process.env.POSTGRES_URL);
+const env = {
+    NODE_ENV: process.env.NODE_ENV ?? 'development',
+    LISTENING_PORT: parseInt(process.env.LISTENING_PORT || '3030'),
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    X_INTERNAL_SECRET: process.env.X_INTERNAL_SECRET
+};
+exports.env = env;
---
+++ express-server/src/env.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import path from "path";
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const envFile = `.env.${nodeEnv}`;
+
+dotenv.config({
+    path: path.resolve(process.cwd(), envFile)
+  });
+
+if (!process.env.POSTGRES_URL) {
+    throw new Error(`POSTGRES_URL is missing in ${envFile}!`);
+}
+
+console.log('Using DB host:', new URL(process.env.POSTGRES_URL).hostname);
+console.log("DB URL OK:", !!process.env.POSTGRES_URL);
+
+const env = {
+    NODE_ENV: process.env.NODE_ENV ?? 'development',
+    LISTENING_PORT: parseInt(process.env.LISTENING_PORT || '3030'),
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    X_INTERNAL_SECRET: process.env.X_INTERNAL_SECRET
+};
+
+export {
+    env
+}
